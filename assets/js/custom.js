fef429cbb61454da740438bfab5c7e5faf7f28eb
Haider Ali <workhaider0@gmail.com>
2021-12-31 00:30:07 +0500

    done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,5 @@
+
+
 (function ($) {
 	
 	"use strict";
@@ -179,7 +181,7 @@
 	});
 
 	$(document).ready(function () {
-	    $(document).on("scroll", onScroll);
+	 
 	    
 	    //smoothscroll
 	    $('.scroll-to-section a[href^="#"]').on('click', function (e) {
@@ -198,25 +200,11 @@
 	            scrollTop: (target.offset().top) - 79
 	        }, 500, 'swing', function () {
 	            window.location.hash = target;
-	            $(document).on("scroll", onScroll);
+	           
 	        });
 	    });
 	});
 
-	function onScroll(event){
-	    var scrollPos = $(document).scrollTop();
-	    $('.nav a').each(function () {
-	        var currLink = $(this);
-	        var refElement = $(currLink.attr("href"));
-	        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-	            $('.nav ul li a').removeClass("active");
-	            currLink.addClass("active");
-	        }
-	        else{
-	            currLink.removeClass("active");
-	        }
-	    });
-	}
 
 
 	// Page loading animation
